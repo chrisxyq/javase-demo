19ae73208699b1a40a2757c7c7fdebb1e426a349
chrisxyq <yuanqixu@trip.com>
2022-03-09 11:11:56 +0800

    init

---
+++ src/test/java/MyDateUtilsTest.java
@@ -0,0 +1,70 @@
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
+import utils.MyDateUtils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MyDateUtilsTest {
+    @Test
+    public void test() {
+        Calendar calendar = null;
+        Date date = MyDateUtils.stringToDate("2022-01-04", "yyyy-MM-dd");
+        if (date != null) {
+            calendar = Calendar.getInstance();
+            calendar.setTime(date);
+        }
+        System.out.println(calendar);
+    }
+    @Test
+    public void test1() {
+        Calendar calendar = null;
+        Date date = MyDateUtils.stringToDate("2022-01-04", "yyyy-MM-dd");
+        if (date != null) {
+            calendar = Calendar.getInstance();
+            calendar.setTime(date);
+        }
+        System.out.println(calendar);
+        System.out.println(MyDateUtils.calendarToString(calendar,"yyyy-MM-dd HH:mm"));
+        System.out.println(MyDateUtils.calendarToString(calendar,"yyyy-MM-dd HH:mm:ss"));
+    }
+    @Test
+    public void test2() {
+        Calendar calendar = null;
+        Date date = MyDateUtils.stringToDate("2022-01-04" + " 23:59:59", "yyyy-MM-dd HH:mm:ss");
+        if (date != null) {
+            calendar = Calendar.getInstance();
+            calendar.setTime(date);
+        }
+        System.out.println(calendar);
+        System.out.println(MyDateUtils.calendarToString(calendar,"yyyy-MM-dd HH:mm"));
+        System.out.println(MyDateUtils.calendarToString(calendar,"yyyy-MM-dd HH:mm:ss"));
+    }
+    @Test
+    public void test3() {
+        Calendar calendar = null;
+        Date date = MyDateUtils.stringToDate("2022-01-04" + " 23:59:59", "yyyy-MM-dd hh:mm:ss");
+        if (date != null) {
+            calendar = Calendar.getInstance();
+            calendar.setTime(date);
+        }
+        System.out.println(calendar);
+        System.out.println(MyDateUtils.calendarToString(calendar,"yyyy-MM-dd hh:mm"));
+        System.out.println(MyDateUtils.calendarToString(calendar,"yyyy-MM-dd hh:mm:ss"));
+    }
+    @Test
+    public void test4() {
+        Date startDate = new Date();
+        startDate = DateUtils.addDays(startDate, -90);
+        startDate = DateUtils.addHours(startDate, 8);
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(startDate);
+    }
+    @Test
+    public void test5() {
+        Date startDate = new Date();
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(startDate);
+        System.out.println(MyDateUtils.calendarToString(startCalendar, "yyyy/MM/dd"));
+    }
+}
--- src/test/java/TempTest.java
+++ src/test/java/TempTest.java
@@ -1,5 +1,8 @@
 import entity.ComplexPerson;
+import entity.MemberCard;
 import entity.Person;
+import enums.OfferTypeEnum;
+import javafx.util.Pair;
 import org.junit.Test;
 import utils.StringUtils;
 
@@ -10,10 +13,11 @@ import java.util.List;
 public class TempTest {
     @Test
     public void test1() {
-        System.out.println(String.format("%s,%s","1","2"));
-        System.out.println(String.format("%s","1"));
-        System.out.println(String.format("'%s',%s","1","2"));
+        System.out.println(String.format("%s,%s", "1", "2"));
+        System.out.println(String.format("%s", "1"));
+        System.out.println(String.format("'%s',%s", "1", "2"));
     }
+
     @Test
     public void test2() {
         System.out.println(StringUtils.trimFirstAndLastChar(",,%s,%s,", ",").split(","));
@@ -21,10 +25,55 @@ public class TempTest {
         System.out.println("b".compareTo("b"));
         System.out.println("b".compareTo("a"));
     }
+
     @Test
     public void test3() {
         List<ComplexPerson> complexPersonList = new ArrayList<>(Arrays.asList(new ComplexPerson(1, new Person("zhangsan"))));
         complexPersonList.get(0).getPerson().setName("zhangsanchanged");
         System.out.println(complexPersonList.get(0).getPerson().getName());
+    }
+
+    @Test
+    public void test4() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(OfferTypeEnum.getEnum(2).getValue());
+        buffer.append(OfferTypeEnum.getEnum(3).getValue());
+        System.out.println(buffer.toString());
+    }
+
+    /**
+     * 不可以对null进行for循环
+     */
+    @Test
+    public void test5() {
+        MemberCard card = new MemberCard("levelName", null);
+        for (Integer integer : card.getApplyLevelList()) {
+            System.out.println(integer);
+        }
+    }
+
+    @Test
+    public void test6() {
+        System.out.println(String.format("%s(%s)", "a", "b"));
+        System.out.printf("%s(%s)%n", "a", "b");
+        System.out.printf("%s(%s)%n", "1", "2");
+    }
+
+    public void encrypt(String encrypted) {
+        encrypted = "encrypted";
+    }
+
+    /**
+     * 在方法内调用更改string的值
+     * 需要传出后，才生效
+     */
+    @Test
+    public void test7() {
+        String encrypted = null;
+        encrypt(encrypted);
+        System.out.println(encrypted);
+    }
+    @Test
+    public void test8() {
     }
 }
